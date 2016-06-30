c5caaff4e9cf6613d841d8766284ce52eb27eda8
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-06-30 18:36:26 +0530

    Fixing typos

--- src/main/java/in/ashwanthkumar/gocd/actions/Action.java
+++ src/main/java/in/ashwanthkumar/gocd/actions/Action.java
@@ -3,8 +3,8 @@ package in.ashwanthkumar.gocd.actions;
 import java.io.File;
 
 /**
- * Action to invoke when a pipeline is not whitelisted.
- * Intead of deleting we could move the artifaccts to another directory - Useful espcially doing it the first time.
+ * Action to invoke when a pipeline is not white listed.
+ * Instead of deleting we could move the artifacts to another directory - Useful especially doing it the first time.
  */
 public interface Action {
     /**
